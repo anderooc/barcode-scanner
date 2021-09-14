e9ea1919e6dc210b0b6e194062a6e91d231a40fb
anderooc <andrew.chang.22@bishops.com>
2021-09-14 09:23:34 -0700

    Image loading and grayscaling image.

---
+++ barcodeScan.py
@@ -0,0 +1,13 @@
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image file")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
