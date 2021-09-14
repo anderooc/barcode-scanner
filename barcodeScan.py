a0500808cc953bd9f951b74ac46e716ee6507d61
anderooc <andrew.chang.22@bishops.com>
2021-09-14 09:28:22 -0700

    Used Sobel operator to compute gradient.

--- barcodeScan.py
+++ barcodeScan.py
@@ -11,4 +11,11 @@ image = cv2.imread(args["image"])
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image = cv2.GaussianBlur(image, (5, 5), 0)
 
+sobelX = cv2.Sobel(image, cv2.CV_64F, 1, 0)
+sobelY = cv2.Sobel(image, cv2.CV_64F, 0, 1)
 
+gradient = cv2.subtract(sobelX, sobelY)
+gradient = cv2.convertScaleAbs(gradient)
+
+cv2.imshow("Gradient", gradient)
+cv2.waitKey(0)
